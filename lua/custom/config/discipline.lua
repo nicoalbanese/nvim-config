2f810483218ff675cb6bf429fe62d52178e8810a
Nico Albanese <gcalbanese96@gmail.com>
2024-01-17 21:24:15 +0000

    added cloak for secret env and discipline for improving habits

--- lua/custom/config/discipline.lua
+++ lua/custom/config/discipline.lua
@@ -12,7 +12,7 @@ function M.cowboy()
             if vim.v.count > 0 then
                 count = 0
             end
-            if count >= 10 then
+            if count >= 20 then
                 ok, id = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
                     icon = "🤠",
                     replace = id,
